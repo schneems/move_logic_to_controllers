9366ac3c665b34a8ccea22013d17a02d16da86b5
Jaci <Jaci@youremail.com>
2012-07-02 19:05:33 -0500

    homework section 2 completed.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,27 @@
 class ProductsController < ApplicationController
+	
 	def index
 		@products = Product.includes(:user).all
 		
 	end
 
 	def create
-  	@product = Product.new(params[:product])
+  		@product = Product.new(params[:product])
 
-  	respond_to do |format|
-    if @product.save
-      format.html { render :action => "create" }
-      format.json { render :json => @product }
-    else
-      format.html { render :action => "new" }
-      format.json { render :json => @product.errors, :status => :unprocessable_entity }
-    end
-  end
-end
+  		respond_to do |format|
+    		if @product.save
+     		 format.html { render :action => "create" }
+     		 format.json { render :json => @product }
+   			else
+      		 format.html { render :action => "new" }
+    		 format.json { render :json => @product.errors, :status => :unprocessable_entity }
+   			end
+ 		end
+	end
+
+	def new
+  		@product = Product.new
+	end
+
+
 end
