a4de5b82fa0088575812ae29b0f934e349e5c1fd
Herminio Torres <herminiocesar@gmail.com>
2013-06-11 01:40:24 -0300

    1 - Move SQL to the Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.includes(:user).all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @products }
+    end
+  end
+
+  def create
+    @product = Product.new(params[:product])
+
+    respond_to do |format|
+      if @product.save
+        format.html { render :action => "create" }
+        format.json { render :json => @product }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
