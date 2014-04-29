b954506e37958c32600d5ca3910073047a148e67
Taaha Chaudhry <t44h413@gmail.com>
2014-04-29 16:32:02 -0500

    Rails Form Helpers/Using partials and routes resources.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,4 +25,36 @@ class ProductsController < ApplicationController
       end
     end
   end
+
+  def show
+    @product = Product.where(:id => params[:id]).first
+  end
+
+  def edit
+    @product = Product.where(:id => params[:id]).first
+  end
+
+  def update
+    @product = Product.where(:id => params[:id]).first
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, :notice => 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product = Product.where(:id => params[:id]).first
+    @product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 ControllerExercise::Application.routes.draw do
 
 
-  get '/products' => 'products#index'
-  get '/products/new' => 'products#new'
-  post '/products' => 'products#create'
-
   resources :users
-
+  resources :products
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
