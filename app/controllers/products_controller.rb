236d0ae9e4e4297b4eb4a1240f5dad3b36bd8a6b
Taaha Chaudhry <t44h413@gmail.com>
2014-04-29 15:20:21 -0500

    Added index, new, and create methods to product controller, and updated product view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,28 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.includes(:user).all
+
+    respond_to do |format|
+      format.html # show default view
+      format.json {render :json => @products}
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+
+    respond_to do |format|
+      if @product.save
+        format.html { render :action => "create" }
+        format.json { render :json => @product }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
