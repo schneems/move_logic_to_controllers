01913ef409535d1694393221b0008375403ea708
Stewart Farley <SFarley92@yahoo.com>
2015-07-15 10:00:36 -0700

    week 5 hw final

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,30 @@
 class ProductsController < ApplicationController
+  def destroy
+    @product = Product.where(:id => params[:id]).first
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
+  def update
+    @product = Product.where(:id => params[:id]).first
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, :notice => 'Product was succesfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit"}
+        format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  def edit
+    @product = Product.where(:id => params[:id]).first
+  end
+  def show
+    @product = Product.where(:id => params[:id]).first
+  end
   def new
     @product = Product.new
   end
